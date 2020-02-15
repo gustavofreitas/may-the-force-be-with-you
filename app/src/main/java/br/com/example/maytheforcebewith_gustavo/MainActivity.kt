f87f217b1d0b0706c4a3163faf304f1e11d19c84
Gustavo Freitas <gustavopozes.freitas@gmail.com>
2020-02-15 20:51:30 -0300

    initial project configuration
    Project creation on Android Studio.
    Dependecies.gradle file cration to prepare the project for modularization.

---
+++ app/src/main/java/br/com/example/maytheforcebewith_gustavo/MainActivity.kt
@@ -0,0 +1,12 @@
+package br.com.example.maytheforcebewith_gustavo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
